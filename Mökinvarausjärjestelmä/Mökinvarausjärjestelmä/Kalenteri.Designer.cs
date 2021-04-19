bb0fd35ee43d1b390024e9094a1abddb39687f11
KarBo <Karbob125@gmail.com>
2021-04-19 18:43:57 +0300

    kalenterin sql koodia (ei toimintoja)

--- Mökinvarausjärjestelmä/Mökinvarausjärjestelmä/Kalenteri.Designer.cs
+++ Mökinvarausjärjestelmä/Mökinvarausjärjestelmä/Kalenteri.Designer.cs
@@ -32,15 +32,13 @@
             this.KalenteriPaneeli = new System.Windows.Forms.Panel();
             this.KalenteriLayoutPaneeliVaraus = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.lblMokkiVaraukset = new System.Windows.Forms.Label();
-            this.lblAlku = new System.Windows.Forms.Label();
             this.lblLoppu = new System.Windows.Forms.Label();
-            this.btnHae = new System.Windows.Forms.Button();
+            this.lblAlku = new System.Windows.Forms.Label();
             this.dateTimePickerAloitus = new System.Windows.Forms.DateTimePicker();
+            this.btnHae = new System.Windows.Forms.Button();
             this.dateTimePickerLopetus = new System.Windows.Forms.DateTimePicker();
-            this.vNDataset = new Mökinvarausjärjestelmä.VNDataset();
-            this.varausBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.varausTableAdapter = new Mökinvarausjärjestelmä.VNDatasetTableAdapters.varausTableAdapter();
+            this.lblMokkiVaraukset = new System.Windows.Forms.Label();
+            this.TbHaku = new System.Windows.Forms.TextBox();
             this.dataGridViewVaraukset = new System.Windows.Forms.DataGridView();
             this.varausidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.asiakasidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -49,13 +47,16 @@
             this.vahvistuspvmDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.varattualkupvmDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.varattuloppupvmDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.TbHaku = new System.Windows.Forms.TextBox();
+            this.varausBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.vNDataset = new Mökinvarausjärjestelmä.VNDataset();
+            this.varausTableAdapter = new Mökinvarausjärjestelmä.VNDatasetTableAdapters.varausTableAdapter();
+            this.hakuToolStripButton = new System.Windows.Forms.ToolStripButton();
             this.KalenteriPaneeli.SuspendLayout();
             this.KalenteriLayoutPaneeliVaraus.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.vNDataset)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.varausBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVaraukset)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.varausBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vNDataset)).BeginInit();
             this.SuspendLayout();
             // 
             // KalenteriPaneeli
@@ -64,7 +65,7 @@
             this.KalenteriPaneeli.Dock = System.Windows.Forms.DockStyle.Fill;
             this.KalenteriPaneeli.Location = new System.Drawing.Point(0, 0);
             this.KalenteriPaneeli.Name = "KalenteriPaneeli";
-            this.KalenteriPaneeli.Size = new System.Drawing.Size(784, 761);
+            this.KalenteriPaneeli.Size = new System.Drawing.Size(797, 761);
             this.KalenteriPaneeli.TabIndex = 0;
             // 
             // KalenteriLayoutPaneeliVaraus
@@ -78,7 +79,7 @@
             this.KalenteriLayoutPaneeliVaraus.RowCount = 2;
             this.KalenteriLayoutPaneeliVaraus.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.5441F));
             this.KalenteriLayoutPaneeliVaraus.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 72.4559F));
-            this.KalenteriLayoutPaneeliVaraus.Size = new System.Drawing.Size(760, 737);
+            this.KalenteriLayoutPaneeliVaraus.Size = new System.Drawing.Size(783, 737);
             this.KalenteriLayoutPaneeliVaraus.TabIndex = 0;
             // 
             // tableLayoutPanel1
@@ -87,7 +88,7 @@
             this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 171F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 172F));
             this.tableLayoutPanel1.Controls.Add(this.lblLoppu, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.lblAlku, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.dateTimePickerAloitus, 0, 1);
@@ -95,7 +96,7 @@
             this.tableLayoutPanel1.Controls.Add(this.dateTimePickerLopetus, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.lblMokkiVaraukset, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.TbHaku, 1, 2);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(14, 3);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 32.29167F));
@@ -104,16 +105,16 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(754, 197);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
-            // lblMokkiVaraukset
+            // lblLoppu
             // 
-            this.lblMokkiVaraukset.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.lblMokkiVaraukset.AutoSize = true;
-            this.lblMokkiVaraukset.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMokkiVaraukset.Location = new System.Drawing.Point(3, 157);
-            this.lblMokkiVaraukset.Name = "lblMokkiVaraukset";
-            this.lblMokkiVaraukset.Size = new System.Drawing.Size(109, 25);
-            this.lblMokkiVaraukset.TabIndex = 0;
-            this.lblMokkiVaraukset.Text = "Varaukset";
+            this.lblLoppu.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblLoppu.AutoSize = true;
+            this.lblLoppu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoppu.Location = new System.Drawing.Point(366, 10);
+            this.lblLoppu.Name = "lblLoppu";
+            this.lblLoppu.Size = new System.Drawing.Size(141, 25);
+            this.lblLoppu.TabIndex = 2;
+            this.lblLoppu.Text = "Lopetus PVM";
             // 
             // lblAlku
             // 
@@ -126,16 +127,14 @@
             this.lblAlku.TabIndex = 1;
             this.lblAlku.Text = "Aloitus PVM";
             // 
-            // lblLoppu
+            // dateTimePickerAloitus
             // 
-            this.lblLoppu.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.lblLoppu.AutoSize = true;
-            this.lblLoppu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLoppu.Location = new System.Drawing.Point(366, 10);
-            this.lblLoppu.Name = "lblLoppu";
-            this.lblLoppu.Size = new System.Drawing.Size(141, 25);
-            this.lblLoppu.TabIndex = 2;
-            this.lblLoppu.Text = "Lopetus PVM";
+            this.dateTimePickerAloitus.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dateTimePickerAloitus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerAloitus.Location = new System.Drawing.Point(7, 49);
+            this.dateTimePickerAloitus.Name = "dateTimePickerAloitus";
+            this.dateTimePickerAloitus.Size = new System.Drawing.Size(276, 26);
+            this.dateTimePickerAloitus.TabIndex = 4;
             // 
             // btnHae
             // 
@@ -149,15 +148,6 @@
             this.btnHae.UseVisualStyleBackColor = true;
             this.btnHae.Click += new System.EventHandler(this.btnHae_Click);
             // 
-            // dateTimePickerAloitus
-            // 
-            this.dateTimePickerAloitus.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.dateTimePickerAloitus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePickerAloitus.Location = new System.Drawing.Point(7, 49);
-            this.dateTimePickerAloitus.Name = "dateTimePickerAloitus";
-            this.dateTimePickerAloitus.Size = new System.Drawing.Size(276, 26);
-            this.dateTimePickerAloitus.TabIndex = 4;
-            // 
             // dateTimePickerLopetus
             // 
             this.dateTimePickerLopetus.Anchor = System.Windows.Forms.AnchorStyles.Top;
@@ -167,19 +157,27 @@
             this.dateTimePickerLopetus.Size = new System.Drawing.Size(277, 26);
             this.dateTimePickerLopetus.TabIndex = 5;
             // 
-            // vNDataset
+            // lblMokkiVaraukset
             // 
-            this.vNDataset.DataSetName = "VNDataset";
-            this.vNDataset.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            this.lblMokkiVaraukset.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblMokkiVaraukset.AutoSize = true;
+            this.lblMokkiVaraukset.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMokkiVaraukset.Location = new System.Drawing.Point(3, 157);
+            this.lblMokkiVaraukset.Name = "lblMokkiVaraukset";
+            this.lblMokkiVaraukset.Size = new System.Drawing.Size(109, 25);
+            this.lblMokkiVaraukset.TabIndex = 0;
+            this.lblMokkiVaraukset.Text = "Varaukset";
             // 
-            // varausBindingSource
+            // TbHaku
             // 
-            this.varausBindingSource.DataMember = "varaus";
-            this.varausBindingSource.DataSource = this.vNDataset;
-            // 
-            // varausTableAdapter
-            // 
-            this.varausTableAdapter.ClearBeforeFill = true;
+            this.TbHaku.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TbHaku.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbHaku.Location = new System.Drawing.Point(300, 157);
+            this.TbHaku.Name = "TbHaku";
+            this.TbHaku.Size = new System.Drawing.Size(273, 26);
+            this.TbHaku.TabIndex = 6;
+            this.TbHaku.Text = "Vapaa Haku Sana";
+            this.TbHaku.Enter += new System.EventHandler(this.TbHaku_Enter);
             // 
             // dataGridViewVaraukset
             // 
@@ -198,7 +196,7 @@
             this.dataGridViewVaraukset.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridViewVaraukset.Location = new System.Drawing.Point(3, 206);
             this.dataGridViewVaraukset.Name = "dataGridViewVaraukset";
-            this.dataGridViewVaraukset.Size = new System.Drawing.Size(754, 528);
+            this.dataGridViewVaraukset.Size = new System.Drawing.Size(777, 528);
             this.dataGridViewVaraukset.TabIndex = 2;
             // 
             // varausidDataGridViewTextBoxColumn
@@ -243,21 +241,30 @@
             this.varattuloppupvmDataGridViewTextBoxColumn.HeaderText = "varattu_loppupvm";
             this.varattuloppupvmDataGridViewTextBoxColumn.Name = "varattuloppupvmDataGridViewTextBoxColumn";
             // 
-            // TbHaku
+            // varausBindingSource
             // 
-            this.TbHaku.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.TbHaku.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TbHaku.Location = new System.Drawing.Point(300, 157);
-            this.TbHaku.Name = "TbHaku";
-            this.TbHaku.Size = new System.Drawing.Size(273, 26);
-            this.TbHaku.TabIndex = 6;
-            this.TbHaku.Text = "Vapaa Haku Sana";
+            this.varausBindingSource.DataMember = "varaus";
+            this.varausBindingSource.DataSource = this.vNDataset;
+            // 
+            // vNDataset
+            // 
+            this.vNDataset.DataSetName = "VNDataset";
+            this.vNDataset.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // varausTableAdapter
+            // 
+            this.varausTableAdapter.ClearBeforeFill = true;
+            // 
+            // hakuToolStripButton
+            // 
+            this.hakuToolStripButton.Name = "hakuToolStripButton";
+            this.hakuToolStripButton.Size = new System.Drawing.Size(23, 23);
             // 
             // Kalenteri
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.ClientSize = new System.Drawing.Size(797, 761);
             this.Controls.Add(this.KalenteriPaneeli);
             this.Name = "Kalenteri";
             this.Text = "Kalenteri";
@@ -266,9 +273,9 @@
             this.KalenteriLayoutPaneeliVaraus.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.vNDataset)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.varausBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVaraukset)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.varausBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vNDataset)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -288,6 +295,7 @@
         private System.Windows.Forms.BindingSource varausBindingSource;
         private VNDatasetTableAdapters.varausTableAdapter varausTableAdapter;
         private System.Windows.Forms.DataGridView dataGridViewVaraukset;
+        private System.Windows.Forms.TextBox TbHaku;
         private System.Windows.Forms.DataGridViewTextBoxColumn varausidDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn asiakasidDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn mokkimokkiidDataGridViewTextBoxColumn;
@@ -295,6 +303,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn vahvistuspvmDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn varattualkupvmDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn varattuloppupvmDataGridViewTextBoxColumn;
-        private System.Windows.Forms.TextBox TbHaku;
+        private System.Windows.Forms.ToolStripButton hakuToolStripButton;
     }
 }
--- Mökinvarausjärjestelmä/Mökinvarausjärjestelmä/Kalenteri.cs
+++ Mökinvarausjärjestelmä/Mökinvarausjärjestelmä/Kalenteri.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
+using System.Data.Sql;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Odbc;
 
 namespace Mökinvarausjärjestelmä
 {
     public partial class Kalenteri : Form
     {
+
         public Kalenteri()
         {
+
             InitializeComponent();
         }
 
@@ -26,9 +31,79 @@ namespace Mökinvarausjärjestelmä
 
         }
 
+        private void TbHaku_Enter(object sender, EventArgs e)
+        {
+            if (TbHaku.Text == "Vapaa Haku Sana")
+            {
+                TbHaku.Text = "";
+            }
+        }
+
         private void btnHae_Click(object sender, EventArgs e)
         {
 
+            
+            if (TbHaku.Text != "")
+            {
+                if (TbHaku.Text == "Vapaa Haku Sana")
+                {
+                    TbHaku.Text = "0";
+                }
+
+                //
+                // tapa käyttäen uutta ODBC yhteyttä
+                //
+                string hsana = ((TbHaku.Text).ToLower()).Replace(" ", "");                      // Hakusana SQL lauseelle
+                string sqlClause = $"SELECT * FROM varaus WHERE varaus_id LIKE '%{hsana}%';";   // SQL lause Stringissä
+
+                OdbcConnection connection = new OdbcConnection("Dsn=Village Newbies");          // yhteys String
+
+                Console.WriteLine(sqlClause);                                                   // printtaa vaa lauseen consolii
+                OdbcCommand cmd = new OdbcCommand(sqlClause, connection);                       // luo komennon lauseista ja yhteydestä
+                try
+                {
+                    connection.Open();                                                          // Yhteyden avaaminen
+                                                                                                // Lukija datalle ja data tulee komennosta josta suoritetaan luku
+                    OdbcDataReader reader = cmd.ExecuteReader(); 
+
+                    while (reader.Read())                                                       // Readeri lukee datan joka tulee Tietokannasta
+                    {
+                        Console.WriteLine("Löytyy!");
+                        for (int j = 0; j < 7; j++)
+                        {
+                            Console.Write(reader.GetName(j));                                   // printataan consoliin alustavasti löytynyt data GetName yms löytää taulusta datan...
+                            Console.Write(" -> ");
+                            Console.Write(reader.GetValue(j));
+                            Console.WriteLine();
+                        }
+                    }
+                    Console.WriteLine(" -> "); // nuoli :D
+                //
+                // Toinen tapa käyttäen Adapteria
+                //
+                    DataTable dt = varausTableAdapter.GetData();                                // Hakee datan Adapterista joka on siis vaa yhteys datasettiin joka taas Tietokantaan
+                    //this.varausTableAdapter.FillBy();
+                    
+                    
+                    foreach (DataRow row in dt.Rows)
+                    {                                                                           // Tämä koodi ei toimi haussa juuri nyt
+                        Console.WriteLine(row["varaus_id"]);                                    // Tässä tapauksessa Data pitää lukea läpi täsmäävällä Tiedon nimellä
+                        Console.WriteLine(row["asiakas_id"]);
+                        Console.WriteLine(row["mokki_mokki_id"]);
+                        Console.WriteLine(row["varattu_pvm"]);
+                    }
+                    varausBindingSource.Filter = $"varaus_id = {hsana}";                        // filteröinti toimii :D binding sourcesta
+                    reader.Close();                                                             // Yhteyksien sulkeminen
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
         }
+
     }
 }
