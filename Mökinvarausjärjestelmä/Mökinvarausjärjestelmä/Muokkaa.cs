0b968fb63ea84e67b7bd37ff7090d3d52aed4e84
Pahkiz <pasanen.juho@gmail.com>
2021-04-29 14:25:07 +0300

    lisäyksiä muokkaatauluun

--- Mökinvarausjärjestelmä/Mökinvarausjärjestelmä/Muokkaa.cs
+++ Mökinvarausjärjestelmä/Mökinvarausjärjestelmä/Muokkaa.cs
@@ -19,6 +19,8 @@ namespace Mökinvarausjärjestelmä
 
         private void Muokkaa_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'vNDataset.mokki' table. You can move, or remove it, as needed.
+            this.mokkiTableAdapter.Fill(this.vNDataset.mokki);
             // TODO: This line of code loads data into the 'vNDataset.palvelu' table. You can move, or remove it, as needed.
             this.palveluTableAdapter.Fill(this.vNDataset.palvelu);
             // TODO: This line of code loads data into the 'vNDataset.lasku' table. You can move, or remove it, as needed.
@@ -27,13 +29,25 @@ namespace Mökinvarausjärjestelmä
         }
 
        
-        private void btnLisaaPalvelu_Click(object sender, EventArgs e)
+        private void btnLisaaPalvelu_Click(object sender, EventArgs e) //lisää palvelun tietokantaan palvelut välilehdessä, kun välilehden tekstikenttiin on lisätty pyydetyt tiedot
         {
             Validate();
             palveluBindingSource.EndEdit();
             palveluTableAdapter.Update(this.vNDataset);
             palveluTableAdapter.Insert(long.Parse(tbPalvelu_id.Text), long.Parse(tbToimintaAlue_id.Text), tbNimi.Text, int.Parse(tbTyyppi.Text), tbKuvaus.Text, double.Parse(tbHinta.Text), double.Parse(tbAlv.Text));
-            
+         
+
+        }
+
+        
+        private void btnPoistaPalvelu_Click(object sender, EventArgs e) //Poistaa valitun rivin Palvelut välilehden DataGridWievistä 
+        {
+            int rowIndex = dgwPalvelut.CurrentCell.RowIndex;
+            dgwPalvelut.Rows.RemoveAt(rowIndex);
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
         }
     }
